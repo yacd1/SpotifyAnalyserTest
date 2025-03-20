2a2ea4bd998d80b227664f5ec6a4c41e1383de82
yachn <iamyachna@gmail.com>
2025-03-20 13:35:37 +0000

    Added a simple React test, to check the app loads with no errors The CI pipeline errors should be fixed

--- spotify-analyzer/src/App.js
+++ spotify-analyzer/src/App.js
@@ -1,10 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Navigate
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 

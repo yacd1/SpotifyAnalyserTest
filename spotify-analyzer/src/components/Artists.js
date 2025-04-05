3ab7e915e7d670df58187cc036ea5010f879598e
yachn <iamyachna@gmail.com>
2025-04-05 19:14:06 +0100

    creating artist page specific css

--- spotify-analyzer/src/components/Artists.js
+++ spotify-analyzer/src/components/Artists.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { apiService } from '../services/api';
 import '../App.css';
+import '../Artists.css';
 import '../Home.css';
 
 function Artists() {

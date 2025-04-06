f2bf105265321221a27e8cae2e99da6268d4593a
yachn <iamyachna@gmail.com>
2025-04-06 16:27:46 +0100

    removing commented code

--- backend/src/main/java/com/spotifyanalyzer/backend/authservice/PythonService.java
+++ backend/src/main/java/com/spotifyanalyzer/backend/authservice/PythonService.java
@@ -10,31 +10,6 @@ import java.net.URL;
 public class PythonService {
     private static final String USER_AGENT = "Mozilla/5.0";
 
-//    public void sendGET(String artistID) throws IOException {
-//        System.out.println("Sending GET request to Python server...");
-//        URL obj = new URL("http://127.0.0.1:5000/getArtistSummary/" + artistID);
-//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//        con.setRequestMethod("GET");
-//        con.setRequestProperty("User-Agent", USER_AGENT);
-//        int responseCode = con.getResponseCode();
-//        System.out.println("GET Response Code :: " + responseCode);
-//        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-//            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//
-//            while ((inputLine = in.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            in.close();
-//
-//            // print result
-//            System.out.println(response.toString());
-//        } else {
-//            System.out.println("GET request did not work.");
-//        }
-//    }
-
     public String sendPOST(String jsonInputString) throws IOException {
 
         //TODO:pass url endpoint as a param in the future to make this more reusable

9958c56fe3e53bf275d1da8642a475044d45d8f6
yachn <iamyachna@gmail.com>
2025-04-21 00:07:48 +0100

    remove duplicate add to DB

--- spotify-analyzer/src/components/Home.js
+++ spotify-analyzer/src/components/Home.js
@@ -163,7 +163,6 @@ const Home = () => {
             fetchTopSongs();
             fetchArtistGameHighScore();
             fetchTrackGameHighScore();
-            addUserToDatabase();
         }
     }, [timeRange]);
 
